983d321950a4c3b2a6877f7d1215db84bb007265
Michael Tran <michaelhuytran@gmail.com>
2017-11-06 21:02:35 -0800

    .

--- src/components/App.js
+++ src/components/App.js
@@ -16,26 +16,26 @@ class App extends Component {
 	}
 }
 
-function mapStateToProps(calendar){
+function mapStateToProps({food, calendar}){
 	const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 	return {
 		calendar: dayOrder.map((day) => ({
 			day,
-				meals: Object.key(calendar[day]).reduce((meals, meal) => {
+			meals: Object.keys(calendar[day]).reduce((meals, meal) => {
 					meals[meal] = calendar[day][meal]
-						? calendar[day][meal]
+						? food[calendar[day][meal]]
 						: null;
 
 					return meals;
-				}, {})
+			}, {})
 		}))
 	}
 }
 
 function mapDispatchToProps( dispatch){
 	return {
-		selectRecipe: (data) => dispatch(addRecipe((data))),
+		selectRecipe: (data) => dispatch(addRecipe(data)),
 		remove: (data) => dispatch(removeFromCalendar(data)),
 	}
 }
